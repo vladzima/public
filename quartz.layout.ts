2a244c235fbdef9cd18e935eb16624a7acef3258
Vlad Arbatov <v.o.arbatov@gmail.com>
2025-02-13 00:09:24 +0300

    Update content

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      Site: "https://arbatov.dev",
+      Blog: "https://blog.arbatov.dev",
+      Twitter: "https://x.com/vladzima",
     },
   }),
 }
